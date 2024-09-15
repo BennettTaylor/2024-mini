735eecc82cc17dc4c7e74c83a4292cc733031852
BennettTaylor <97144368+BennettTaylor@users.noreply.github.com>
2024-09-15 14:37:43 -0400

    API now pushes game data to firestore cloud database

--- API/app/__init__.py
+++ API/app/__init__.py
@@ -4,21 +4,30 @@
 # Required Imports
 import os
 from flask import Flask, request, jsonify
-from firebase_admin import credentials, firestore, initialize_app
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+username = "newtest"
 
 # Initialize Flask App
 app = Flask(__name__)
 # Initialize Firestore DB
 cred = credentials.Certificate('key.json')
-default_app = initialize_app(cred)
-db = firestore.client()
 
-response_time_data_ref = db.collection('response-time-data')
+firebase_app = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
 
 @app.route('/data', methods=['POST'])
 def upload():
     try:
         average = request.json['average']
-        print(average)
+        minimum = request.json['minimum']
+        maximum = request.json['maximum']
+        score = request.json['score']
+        doc_ref = db.collection("response-time-data").document(username)
+        doc_ref.set({"Average": average, "Maximum": maximum, "Minimum": minimum, "Score": score})
+        return "Sucess!"
     except Exception as e:
         return f"An Error Occured: {e}"
